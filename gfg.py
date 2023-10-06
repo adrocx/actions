fd941e7ec4da9c2d433f510c244c074b474658bd
Aditya Raju <adrocx@Adityas-MacBook-Air-3.local>
2023-10-06 16:23:22 +0530

    6_10_23_assgn

---
+++ gfg.py
@@ -0,0 +1,17 @@
+"""
+This is a simple Python script.
+"""
+
+# Constants
+A = 1
+B = 2
+
+def main():
+    """
+    The main function.
+    """
+    result = A + B
+    print(result)
+
+if __name__ == "__main__":
+    main()
